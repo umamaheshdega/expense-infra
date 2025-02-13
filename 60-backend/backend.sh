2b44cc6d32bc056b8c4631e8bd274f098ad3cf05
sivakumarreddym <info@joindevops.com>
2025-02-13 09:42:34 +0530

    S3 PART-02

---
+++ 60-backend/backend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dnf install ansible -y
+# push
+# ansible-playbook -i inventory mysql.yaml
+
+#pull
+ansible-pull -i localhost, -U https://github.com/DAWS-82S/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
